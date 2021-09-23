7c137d719d73c1d626c6c55e9a3b7a77c0f3578f
Akash <akashharini3@gmail.com>
2021-09-23 21:53:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+    })
+    $('#close').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+    })
+    $('#scrolll').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+    })
+})
